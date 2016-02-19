c7de9b26b910050416a64b50272cfc2ed2494fbd
Amr Kahhaleh <akahhaleh@ucsd.edu>
2016-02-19 12:58:11 -0800

    removed unessarry files

--- routes/all-listings.js
+++ routes/all-listings.js
@@ -1,11 +1,7 @@
 var housingData = require('../housing.json');
-var localesData = require('../locales.json');
-var refugeesData = require('../refugees.json');
 
 exports.view = function(req, res){
 	console.log(housingData);
-	console.log(localesData);
-	console.log(refugeesData);
 
 	res.render('all-listings', housingData);
 };
