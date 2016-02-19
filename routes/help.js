bfcba02541e7ed2973d42457582bc07d577f006e
Amr Kahhaleh <akahhaleh@ucsd.edu>
2016-02-19 13:01:22 -0800

    removed json from landing routes

--- routes/help.js
+++ routes/help.js
@@ -1,6 +1,3 @@
-var housingData = require('../housing.json');
-var localesData = require('../locales.json');
-var refugeesData = require('../refugees.json');
 var helpQuestions = require('../help.json');
 
 exports.view = function(req, res){
--- routes/how-it-works.js
+++ routes/how-it-works.js
@@ -1,6 +1,3 @@
-var housingData = require('../housing.json');
-var localesData = require('../locales.json');
-var refugeesData = require('../refugees.json');
 var tutorialTopics = require('../tutorial.json');
 
 exports.view = function(req, res){
