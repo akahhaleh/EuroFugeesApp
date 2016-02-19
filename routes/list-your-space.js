c08c16cedabb24129aac95afa67812584d10b8a2
Amr Kahhaleh <akahhaleh@ucsd.edu>
2016-02-18 20:53:02 -0800

    removed housin.json from list-your-space

--- routes/list-your-space.js
+++ routes/list-your-space.js
@@ -1,15 +1,5 @@
 var housingData = require('../housing.json');
-var localesData = require('../locales.json');
-var refugeesData = require('../refugees.json');
 
 exports.view = function(req, res){
-<<<<<<< HEAD
 	res.render('list-your-space');	
-=======
-	console.log(housingData);
-	console.log(localesData);
-	console.log(refugeesData);
-
-	res.render('list-your-space', housingData);
->>>>>>> 8cf671ca3b55eff4b70fb5cff9d426a9e998d0dc
 };
