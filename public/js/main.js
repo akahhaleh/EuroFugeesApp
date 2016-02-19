d4d7f90c83bb54db59fa7877934f65d46d2f37ef
James Walrath <jwalrath@ucsd.edu>
2016-02-18 22:00:52 -0800

    Slideshow of listings images

--- public/js/main.js
+++ public/js/main.js
@@ -3,9 +3,31 @@
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
+
+  var counter = 0,
+  $items = $('.listing-slideshow figure'),
+  numItems = $items.length;
+
+  var showCurrent = function(){
+    var itemToShow = Math.abs(counter%numItems);
+    $items.removeClass('show');
+    $items.eq(itemToShow).addClass('show');
+  };
+
+  $('.next').on('click', function(){
+    counter++;
+    showCurrent();
+  });
+
+  $('.prev').on('click', function(){
+    counter--;
+    showCurrent();
+  });
+
 })
 
 function initializePage() {
+
 	$('.help-topic a').click(addHelpAnswer);
 	$('.tutorial-topic a').click(addTutorialAnswer);
 }
