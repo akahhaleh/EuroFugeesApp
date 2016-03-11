97bc320a742322ede8f2e1757c5cd03a7972c19b
Amr Kahhaleh <akahhaleh@ucsd.edu>
2016-03-11 00:40:55 -0800

    trying my-listings attempt 3

---
+++ public/js/myListings.js
@@ -0,0 +1,32 @@
+'use strict';
+
+// Call this function when the page loads (the "ready" event)
+$(document).ready(function() {
+	initializePage();
+})
+
+/*
+ * Function that is called when the document is ready.
+ */
+ function initializePage() {
+   console.log("Javascript connected! - myListing.js");
+   /*
+   var houseCard = document.getElementById("listing");
+   if (document.contains(houseCard)){
+    document.getElementById('query-result').innerHTML="<h3>Results that match your query:</h3>";
+  }else{
+    document.getElementById('query-result').innerHTML = "<h3>No results match your query</h3><p>EuroFugees services are currenlty available to the following cities:</p><ul><li>Berlin</li><li>Munich</li></ul><p>Please limit your inqueries to only these cities.";
+    console.log("found no results");
+ }*/
+}
+
+function registerID(id){
+	console.log("selectedListingID = "+id);
+	localStorage.setItem('selectedListingID', id);
+}
+
+
+
+
+
+
